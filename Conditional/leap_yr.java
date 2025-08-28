71e964d1d598962c6d6897d96db97ac908508128
swethasj12 <swethasenthilnathan.j12@gmail.com>
2025-08-29 00:09:23 +0530

    Create leap_yr.java

---
+++ Conditional/leap_yr.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class leap_yr {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        if((n%400 == 0) || (n%100 != 0) && (n%4 == 0)){
+            System.out.println("Leap year");
+        }
+        else{
+            System.out.println("Not a Leap year");
+        }
+        }
+    
+}
