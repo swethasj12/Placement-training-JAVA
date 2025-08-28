6c413fad5f36f62be19ce93b6c5bcd0411c4cd6d
swethasj12 <swethasenthilnathan.j12@gmail.com>
2025-08-29 00:09:53 +0530

    Create odd_even.java

---
+++ Conditional/odd_even.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class odd_even {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        if(n%2 == 0){
+            System.out.println("EVEN");
+        }
+            else{
+                System.out.println("ODD");
+            }
+        }
+    
+}
