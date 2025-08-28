25da89b97edab024f96ce1f9e151900d20d3fb46
swethasj12 <swethasenthilnathan.j12@gmail.com>
2025-08-29 00:08:51 +0530

    Create equal_num.java

---
+++ Conditional/equal_num.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class equal_num {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+         b = sc.nextInt();
+        if (a == b ){
+            System.out.println("EQUAL");
+        }
+        else
+            System.out.println("NOT EQUAL");
+        }
+}
